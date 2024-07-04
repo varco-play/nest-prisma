d9b142182200718071039c4ec674e1d18bcb28e3
varco-play <privetchel2007@gmail.com>
2024-07-05 04:55:14 +0500

    jwt verification

--- src/auth/auth.middleware.ts
+++ src/auth/auth.middleware.ts
@@ -1,11 +1,19 @@
 import { Injectable, NestMiddleware, UnauthorizedException } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthMiddleware implements NestMiddleware {
+  constructor(private jwt: JwtService) {}
   use(req: any, res: any, next: () => void) {
     const token = req.cookies['auth'];
 
-    if(!token) throw new UnauthorizedException("The user is not authorized")
+    if(!token) throw new UnauthorizedException("The user is not authorized");
+
+     const isValid = this.jwt.verify(token);
+    if (!isValid) {
+      throw new UnauthorizedException('Invalid authentication cookie');
+    }
     next();
   }
 }
